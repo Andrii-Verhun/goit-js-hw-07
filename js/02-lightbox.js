73af4eced77be7c2305dfca678a5ad11821afd3e
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-11 14:12:05 +0200

    Task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const elemGallery = galleryItems.map((image) => {
+    const elemLink = document.createElement('a');
+    elemLink.classList.add('gallery__item');
+    elemLink.href = image.original;
+
+    const elemImg = document.createElement('img');
+    elemImg.classList.add('gallery__image');
+    elemImg.src = image.preview;
+    elemImg.alt = image.description;
+    elemImg.dataset.source = image.original;
+
+    elemLink.append(elemImg);
+    return elemLink;
+});
+
+gallery.append(...elemGallery);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250',
+});
